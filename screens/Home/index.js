0d11f1f10e22d80d570101b10ca21896122d545c
maurogramajo <grama_mauro@live.com.ar>
2023-03-10 17:07:36 -0300

    test

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -139,6 +139,10 @@ function HomeScreen({ route }) {
           fontSize={30}
           bold
         >{i18next.t('home.title')}</Text>
+        <Text
+          fontSize={30}
+          bold
+        >HOLA MUNDO</Text>
         <View style={styles.pictureFrame}>
           <Image
             style={styles.profilePhoto}
